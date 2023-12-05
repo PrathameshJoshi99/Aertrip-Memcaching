f460b131c76b60864ac6ae5f560149d0a6875743
Prathamesh Joshi <118970122+PrathameshJoshi99@users.noreply.github.com>
2023-12-05 19:11:56 +0530

    Create models.py

---
+++ models.py
@@ -0,0 +1,12 @@
+# backend/app/models.py
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Airport(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    code = db.Column(db.String(10), unique=True, nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    city = db.Column(db.String(100), nullable=False)
+    country = db.Column(db.String(100), nullable=False)
